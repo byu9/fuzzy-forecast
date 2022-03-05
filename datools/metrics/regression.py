7f09f81e824c248b7d5553924e4a45cd724ec917
John Yu <byu9@ncsu.edu>
2022-03-05 18:08:23 -0500

    add multiple regression metrics

--- datools/metrics/regression.py
+++ datools/metrics/regression.py
@@ -3,36 +3,90 @@ Regression Metrics
 '''
 
 __all__ = (
-    'Mean_Squared_Error',
-    'Sum_Of_Squared_Error',
+    'mean_absolute_error',
+    'mean_squared_error',
+    'root_mean_squared_error',
+    'sum_of_squared_error',
+    'mean_absolute_percent_error',
+    'mean_absolute_percent_full_scale_error',
+    'mean_bias_error',
+    'coefficient_of_determination',
 )
 
 
 import numpy
 
 
-class Mean_Squared_Error:
+def mean_absolute_error(pred, actual):
+    pred = numpy.asarray(pred)
+    actual = numpy.asarray(actual)
 
-    @staticmethod
-    def __call__(pred, actual):
-        return Mean_Squared_Error.primitive(pred, actual)
+    error = actual - pred
 
-    @staticmethod
-    def primitive(pred, actual):
-        pred = numpy.asarray(pred)
-        actual = numpy.asarray(actual)
+    return abs(error).mean()
 
-        return numpy.square(actual - pred).mean()
+def mean_squared_error(pred, actual):
+    pred = numpy.asarray(pred)
+    actual = numpy.asarray(actual)
 
-class Sum_Of_Squared_Error:
+    error = actual - pred
 
-    @staticmethod
-    def __call__(pred, actual):
-        return Sum_Of_Squared_Error.primitive(pred, actual)
+    return numpy.square(error).mean()
 
-    @staticmethod
-    def primitive(pred, actual):
-        pred = numpy.asarray(pred)
-        actual = numpy.asarray(actual)
 
-        return numpy.square(actual - pred).sum()
+def root_mean_squared_error(pred, actual):
+    return numpy.sqrt(mean_squared_error(pred, actual))
+
+
+def sum_of_squared_error(pred, actual):
+    pred = numpy.asarray(pred)
+    actual =  numpy.asarray(actual)
+
+    error = actual - pred
+
+    return numpy.square(error).sum()
+
+
+def mean_absolute_percent_error(pred, actual):
+    pred = numpy.asarray(pred)
+    actual = numpy.asarray(actual)
+
+    error = actual - pred
+    error_percent = error / pred * 100
+
+    return abs(error_percent).mean()
+
+
+def mean_absolute_percent_full_scale_error(pred, actual, fullscale=None):
+    pred = numpy.asarray(pred)
+    actual = numpy.asarray(actual)
+
+    if fullscale is None:
+        fullscale = abs(actual).max()
+
+    error = actual - pred
+    error_percent = error / fullscale * 100
+
+    return abs(error_percent).mean()
+
+
+def mean_bias_error(pred, actual):
+    pred = numpy.asarray(pred)
+    actual = numpy.asarray(actual)
+
+    error = actual - pred
+
+    return error.mean()
+
+def coefficient_of_determination(pred, actual):
+    pred = numpy.asarray(pred)
+    actual = numpy.asarray(actual)
+
+    error = actual - pred
+
+    residual_sum_of_square = numpy.squre(error).sum()
+    total_sum_of_square = numpy.square(actual).sum()
+
+    return 1 - residual_sum_of_square / total_sum_of_square
+
+
